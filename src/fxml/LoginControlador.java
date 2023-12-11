a49e5e9c7b5a883d9f0fbf75a4f23334ac03d039
leandromenoci <menoci.leandro@gmail.com>
2023-12-10 21:31:02 -0300

    Notifications com controlsfx

--- src/fxml/LoginControlador.java
+++ src/fxml/LoginControlador.java
@@ -1,8 +1,10 @@
 package fxml;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.controlsfx.control.Notifications;
 
 public class LoginControlador {
 
@@ -13,7 +15,20 @@ public class LoginControlador {
     private PasswordField senhaField;
 
     public void entrar() {
-        System.out.println(emailField.getText());
-        System.out.println(senhaField.getText());
+        boolean emailValido = emailField.getText().equals("aluno@cod3r.com.br");
+        boolean senhaValida = senhaField.getText().equals("123456");
+
+        if(emailValido && senhaValida) {
+            Notifications.create()
+                    .title("Login FXML")
+                    .text("Login efetuado com sucesso!")
+                    .showInformation();
+        } else {
+            Notifications.create()
+                    .position(Pos.TOP_RIGHT)
+                    .title("Login FXML")
+                    .text("Ocorreu uma falha no login!")
+                    .showError();
+        }
     }
 }
